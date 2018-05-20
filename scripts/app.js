afa0e61664d56a9af7cbc4ae3258d0cc70b4922b
harishbalamurugan <33050285+harishbalamurugan@users.noreply.github.com>
2018-05-20 14:03:45 +0800

    Update table UI for contents/firewall.html

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+var home_btns = document.querySelectorAll(".btn--home");
+var rule_btns = document.querySelectorAll(".btn--rules");
+
+// Set up path prefix i.e. '../' (for urls links in button actions)
+var path_prefix = "../";
+if (location.pathname.endsWith("index.html")) path_prefix = "";
+
+// Set actions for buttons
+if (home_btns.length !== 0) 
+    for (var i in home_btns) home_btns[i].href = path_prefix + "index.html";
+if (rule_btns.length !== 0) 
+    for (var i in rule_btns) rule_btns[i].href = path_prefix + "contents/firewall.html";
