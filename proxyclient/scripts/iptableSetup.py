2e9e20b1d2ac4c5924e5ac375fb329d4b716c582
Unknown <angjinkuan@hotmail.sg>
2018-05-24 22:42:33 +0800

    Initial scripting (2)

--- proxyclient/scripts/iptableSetup.py
+++ proxyclient/scripts/iptableSetup.py
@@ -1,9 +1,17 @@
 import subprocess
 import json
 import policyparser
+import subprocess
 
-class iptablesHandler:
+class IptablesHandler:
     def flush():
+        subprocess.run(["sudo iptables -P INPUT ACCEPT"])
+        subprocess.run(["sudo iptables -P FORWARD ACCEPT"])
+        subprocess.run(["sudo iptables -P OUTPUT ACCEPT"])
+        subprocess.run(["sudo iptables -t nat -F"])
+        subprocess.run(["sudo iptables -t mangle -F"])
+        subprocess.run(["sudo iptables -F"])
+        subprocess.run(["sudo iptables -X"])
 
     def createRule(rulejson : string):
         //load json
--- proxyclient/scripts/proxyscript.py
+++ proxyclient/scripts/proxyscript.py
@@ -8,9 +8,17 @@ class WebFilter:
         #test using kongregate
         if flow.request.url == "https://www.kongregate.com":
             #stop runnnig immediately
-            sys.exit(1)
+            console.log("Stopped by webfilter!")
+            flow.reply("Permission Denied")
+            flow.response.content = "Permission Denied!"
 
     def response(self, flow: http.HTTPFlow) -> None:
-        #check reponse
+        #open response using BeautifulSoup
+        #stop processing of flow first
+        flow.intercept()
+        html = BeautifulSoup(flow.response.content)
+        #continue if no error
+        flow.resume()
+
 
 addons = [WebFilter()]
