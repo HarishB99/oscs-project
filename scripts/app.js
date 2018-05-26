c7ad65f14c8336b053a8f6f06abf72e7f488d9f7
harishbalamurugan <160390R@mymail.nyp.edu.sg>
2018-05-26 19:11:23 +0800

    Update firewall buttons before mergine latest change from master (README.md)

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,27 @@
 var home_btns = document.querySelectorAll(".btn--home");
 var rule_btns = document.querySelectorAll(".btn--rules");
 var port_inputs = document.querySelectorAll(".rule__ports");
+// Add rule button is to proceed to the "Add Rule" page to add a rule.
+var add_rule_btn = document.querySelector(".firewall-button--add");
+// Delete button is to delete an existing rule.
+var delete_rule_btn = document.querySelector(".firewall-button--delete");
+// Publish button is to publish changes to the firewall.
+var publish_rule_btn = document.querySelector(".firewall-button--publish");
+// Update button is to create/edit a new rule. 
+// Must be explicitly published using the publish button.
+var update_rule_btn = document.querySelector(".firewall-button--update");
+// Cancel button is to cancel the rule creation process.
+var cancel_rule_btn = document.querySelector(".firewall-button--cancel");
 
 // Set up path prefix i.e. '../' (for urls links in button actions)
 var path_prefix = "/";
-// if (location.pathname.endsWith("index.html")) path_prefix = "";
 
 // Set actions for buttons
-if (home_btns.length !== 0) 
-    for (var i in home_btns) home_btns[i].href = path_prefix + "index.html";
-if (rule_btns.length !== 0) 
-    for (var i in rule_btns) rule_btns[i].href = path_prefix + "contents/firewall.html";
+if (home_btns.length !== 0) {
+    for (var i in home_btns) home_btns[i].href = "/";
+} if (rule_btns.length !== 0) {
+    for (var i in rule_btns) rule_btns[i].href = "/firewall";
+}
 
 function isAValidPort(el) {
     var port_enterd = el.value;
@@ -27,4 +38,16 @@ function isAValidPort(el) {
     if (el.classList.contains("is-invalid")) {
         el.classList.remove("is-invalid");
     }
+}
+
+if (add_rule_btn !== null) {
+    add_rule_btn.onclick = function(e) {
+        location.href = "/add_rule";
+    };
+}
+
+if (cancel_rule_btn !== null) {
+    cancel_rule_btn.onclick = function(e) {
+        location.href = "/firewall";
+    };
 }
