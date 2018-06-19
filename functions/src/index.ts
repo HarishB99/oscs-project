89be9e813db4930715fd69ef1457e3eea6404d81
harishbalamurugan <160390R@mymail.nyp.edu.sg>
2018-06-19 12:31:37 +0800

    Add codes to test login functionality

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
 import * as functions from 'firebase-functions';
 import * as firebase from 'firebase-admin'
 import * as express from 'express'
+import * as crypto from 'crypto'
+import { Buffer } from 'buffer';
 
 firebase.initializeApp()
 
@@ -22,16 +24,97 @@ class Rule {
     constructor(public priority: number, public name: string, public allow: boolean, public sourceip: string, public sourceport: string, public destip: string, public destport: string, public protocol: string, public state: string) {}
 }
 
-interface Rule {
-    priority: number
-    allow: boolean
-    sourceip: string
-    sourceport: string
-    destip: string
-    destport: string
-    protocol: string
-    state: string
+async function encrypt(...args: string[]) {
+    let plainText = ''
+    let password = 'toUnlockThis@nypsit'
+    if (args.length > 0) {
+        plainText = args[0]
+    }
+    if (args.length > 1) {
+        password = args[1]
+    }
+    const cipher = crypto.createCipher('aes256', password)
+    let encrypted = cipher.update(plainText, 'utf8', 'hex')
+    encrypted += cipher.final('hex')
+    return encrypted
 }
+
+async function decrypt(...args: string[]) {
+    let cipherText = ''
+    let password = 'toUnlockThis@nypsit'
+    if (args.length > 0) {
+        cipherText = args[0]
+    }
+    if (args.length > 1) {
+        password = args[1]
+    }
+    const cipher = crypto.createDecipher('aes256', password)
+    let decrypted = cipher.update(cipherText, 'hex', 'utf8')
+    decrypted += cipher.final('hex')
+    return decrypted
+}
+
+async function hash(plainText: string) {
+    const hash_computer = crypto.createHash('sha512')
+    hash_computer.update(plainText)
+    return hash_computer.digest('hex')
+}
+
+// app.get('/create-test-user', async (request, response) => {
+//     const salt = crypto.pseudoRandomBytes(64).toString('hex') // Number represents number of bytes
+//     const encryptedSalt = await encrypt(salt)
+//     const password = "secretTestPassword"
+//     const encryptedPassword = await encrypt(password)
+//     const hashedPassword = await hash(encryptedPassword+salt)
+    
+//     try {
+//         const userRecord = await firebase.auth().createUser({
+//             email: "test@example.com",
+//             emailVerified: false,
+//             phoneNumber: "+6587127475",
+//             displayName: "Tester 1",
+//             photoURL: "https://material.io/tools/icons/static/icons/baseline-perm_identity-24px.svg",
+//             disabled: false
+//         })
+//         console.log("Successfully created new user", userRecord.uid)
+//         const writeStatus = await firebase.firestore().doc(`/users/${userRecord.uid}`).set({
+//             email: userRecord.email,
+//             phoneNumber: userRecord.phoneNumber,
+//             displayName: userRecord.displayName,
+//             password: hashedPassword,
+//             salt: encryptedSalt,
+//             emailVerified: userRecord.emailVerified,
+//             disabled: userRecord.disabled,
+//             photoURL: userRecord.photoURL,
+//             organisation: 'example.org'
+//         })
+//         console.log("Status of account write operation to database: ", writeStatus)
+//         response.send('User Creation Successful')
+//     } catch (error) {
+//         console.error("Error creating new user", error)
+//         response.send('Internal Error')
+//     }
+// })
+
+app.get('/login', async (request, response) => {
+    try {
+        const email = request.params.username
+        const pass = request.params.pass
+        const userRecord = await firebase.auth().getUserByEmail(email)
+        const dbUserRecord = await firebase.firestore().doc(`/users/${userRecord.uid}`).get()
+        const salt = await decrypt(dbUserRecord.get('salt'))
+        const encryptedPass = await encrypt(pass)
+        const hashedPass = await hash(encryptedPass + salt)
+        console.log("hashedPass: ", hashedPass)
+        console.log('hashedPassword: ', dbUserRecord.get('password'))
+        if (crypto.timingSafeEqual(Buffer.from(hashedPass), Buffer.from(dbUserRecord.get('password'))))
+            response.send('Your username is ' + dbUserRecord.get('displayName'))
+        else
+            response.send('Access denied')
+    } catch (error) {
+        response.end('Access denied')
+    }
+})
 
 app.get('/rules.json', async (request, response) => {
     // The following line sets the Cache Control such that 
@@ -80,3 +163,11 @@ app.get('/rules.json', async (request, response) => {
 })
 
 export const web_app = functions.https.onRequest(app)
+
+export const createNewUser = functions.auth.user().onCreate(async user => {
+    // const uid = user.uid
+    // const email = user.email
+    // const photoURL = user.photoURL
+    // const phoneNumber = user.phoneNumber
+    console.log("UserRecord created", user)
+})
