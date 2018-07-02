f77e5a826fda6d7a6e762ea7de1be2f7c0a4fd7f
harishbalamurugan <160390R@mymail.nyp.edu.sg>
2018-07-02 10:35:42 +0800

    Change back to Typescript
    - Because Firebase Client SDK has now been implemeneted in clients only, hence no more errors in TypeScript
    - TypeScript helps to simplify the coding process by providing useful suggestions
    - TS helps to also identify bugs while coding

---
+++ functions/src/Input.ts
@@ -0,0 +1,43 @@
+/**
+ * Interface to store user input for processing
+ * @author Harish S/O Balamurugan
+ */
+export class Input {
+    public email: string;
+    public password: string;
+    public phoneNumber: string;
+    public organisation: string;
+    public photoURL: string;
+    public displayName: string;
+    
+    /**
+     * To construct an Input object to process user input
+     * @param {*} email email of user
+     * @param {*} password password of user
+     * @param {*} phoneNumber phoneNumber of user
+     * @param {*} organisation organisation of user
+     * @param {*} photoURL url of the display picture of the user
+     */
+    public constructor(email: string, password: string, phoneNumber: string, organisation: string, photoURL: string) {
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = '+65'.concat(phoneNumber);
+        this.organisation = organisation;
+        this.photoURL = photoURL || 'https://material.io/tools/icons/static/icons/baseline-permidentity-24px.svg';
+        this.displayName = this.email.substr(0, this.email.lastIndexOf('@'));
+    }
+
+    /**
+     * @return a string representation of the Input object
+     */
+    public toString(): string {
+        return JSON.stringify({
+            email: this.email,
+            password: this.password,
+            phoneNumber: this.phoneNumber,
+            organisation: this.organisation,
+            photoURL: this.photoURL,
+            displayName: this.displayName
+        });
+    }
+}
---
+++ functions/src/UserClaim.ts
@@ -0,0 +1,7 @@
+/**
+ * Interface to store user claims
+ * @author Harish S/O Balamurugan
+ */
+export interface UserClaim {
+    organisation: string;
+}
