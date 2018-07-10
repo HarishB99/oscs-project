bf2d2fe45a3bb4efa70da111f75b6c61856e8712
Unknown <angjinkuan@hotmail.sg>
2018-07-10 22:48:51 +0800

    Database class, expand and fix proxy function

--- proxyclient/scripts/db.py
+++ proxyclient/scripts/db.py
@@ -1,5 +1,39 @@
-from pymongo import MongoClient
+import pymongo
 
-client = MongoClient('localhost', 27017)
+class LogDatabase:
+    def __init__(self):
+        self.client = pymongo.MongoClient('localhost', 27017)
+        self.database = client.get_default_database()
+        self.logs = self.database['logs']
 
-db = client.log_database
+    def newUser(ip):
+        user = {
+            "_id": ip,
+            "domains" : {},
+            "events": {
+                "maliciousFile": [],
+                "blockedDomains" : {},
+                "suspiciousDomain": [],
+                "downloadedFile": []
+            }
+        }
+        return true
+
+    def getUser(ip):
+        return self.logs.find_one({"_id":ip})
+
+    def updateUser(u):
+        self.logs.update_one({'_id':u["_id"]}, {"$set": u}, upsert=False)
+
+    def request(ip, domain):
+        u = getUser(ip)
+        if u["domains"][domain] is None:
+            u["domains"][domain] = 1
+        else:
+            u["domains"][domain]++
+        updateUser(u)
+
+    def securityEvent(ip, eventType, details):
+        u = getUser(ip)
+        u["events"][eventType].append(details)
+        updateUser(u)
