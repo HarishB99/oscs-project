d6fa0f871ff9bf3dba0d27a1969d0495caa44865
harishbalamurugan <160390R@mymail.nyp.edu.sg>
2018-07-26 19:57:30 +0800

    Add IV method to validate url

--- functions/src/InputValidator.ts
+++ functions/src/InputValidator.ts
@@ -175,13 +175,24 @@ export class InputValidator {
         return re.test(input);
     }
 
-    private isValidPhotoUrl(photoURL: string): boolean {
-        if (this.isEmpty(photoURL)) return false;
-        // TODO: Create a regular expression to 
-        // check for a valid photo url
-        const re = /^$/;
-        return re.test(photoURL);
+    /**
+     * Check whether the URL can be accepted as a valid value
+     * @param input the string input to be validated
+     */
+    private isValidUrl(input: string): boolean {
+        if (this.isEmpty(input)) return false;
+        const re = /^(?:(?:(?:https?):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/;
+        // /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/
+        return re.test(input);
     }
+
+    // private isValidPhotoUrl(photoURL: string): boolean {
+    //     if (this.isEmpty(photoURL)) return false;
+    //     // TODO: Create a regular expression to 
+    //     // check for a valid photo url
+    //     const re = /^((https?|ftp):)?\/\/.*(jpeg|jpg|png|gif|bmp)$/;
+    //     return re.test(photoURL);
+    // }
 
     /**
      * Checks whether the inputs given to 
