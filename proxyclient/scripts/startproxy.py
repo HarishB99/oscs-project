ffb79bc4da8972ea0852e015d41bf5936e127df3
Unknown <angjinkuan@hotmail.sg>
2018-08-05 15:55:47 +0800

    Linux - Python boolean thing

--- proxyclient/scripts/startproxy.py
+++ proxyclient/scripts/startproxy.py
@@ -9,7 +9,7 @@ from PyQt5 import QtGui
 
 #spawn firegate client server
 os.chdir("../../firegate101-client")
-if !sys.platform.startswith('linux'):
+if not sys.platform.startswith('linux'):
     firegateClientP = subprocess.Popen(["node", "index.js"],
      shell=True)
     atexit.register(firegateClientP.terminate)
